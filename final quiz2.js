28dcbc31cd893be22d14ae2b7f56e863b8c532ec
dmitriyyep <55818941+dmitriyyep@users.noreply.github.com>
2020-01-06 19:32:42 -0800

    Add files via upload

---
+++ final quiz2.js
@@ -0,0 +1,115 @@
+const STORE = {
+    questions: [//1
+      {
+        question: "What type of coffee would you get if you mix: 50% milk, 25% steamed milk, and 25% espresso?",
+        options: [
+          "Americano", 
+          "Flat-White", 
+          "Cappucino", 
+          "Hot-Chocolate"
+        ],
+        answer: "Cappucino"
+      },
+      //2
+      {
+        question: "What type of coffee would you get if you only have espresso in your cup?",
+        options: [
+          "A cup",
+          "A latte", 
+          "A cup of Espresso",
+          "This Question is too easy gimme something harder"
+        ],
+        answer: "A cup of Espresso"
+      },
+      //3
+      {
+        question: "What type of coffee would you get if you mix: 75% water, and 25% Espresso?",
+        options: [
+          "Black Coffee", 
+          "Iced Coffee", 
+          "I think the correct answer is actually Cappucino but that was the answer to question 1...", 
+          "Americano"
+        ],
+        answer: "Americano"
+      },
+      //4
+      {
+        question: "What type of coffee would you get if you mix: 25% coffee, and 25% water?	", 
+        options: [
+          "Now i know for sure this is Black Coffee... i think",
+          "Pumpkin spice",
+          "Caffine",
+          "Almond Milk Coffee",
+        ],
+        answer: "Now i know for sure this is Black Coffee... i think"
+      },
+      //5
+      {
+        question: "What type of coffee would you get if you mix: 75% steamed milk, and 25% espresso?",
+        options: [
+          "Esspresso", 
+          "Hot milk", 
+          "wait, is there such a thing as Hot milk coffee?", 
+          "Cafe Latte"
+        ],
+        answer: "Cafe Latte"
+      },
+      //6
+      {
+        question: "What type of coffee would you get if you mix: 25% sugar, 25% espresso, 50% water?",
+        options: [
+          "Hot Coffee", 
+          "Turkish Coffee", 
+          "White Chocolate ", 
+          "I dont think i will ever drink this type of coffee, like ever?"
+        ],
+        answer: "Turkish Coffee"
+      },
+      //7
+      {
+        question: "What type of coffee would you get if you mix: 50% Steamed milk, and 50% espresso?",
+        options: [
+          "Iced Coffee", 
+          "like TBH this coffee has wayyy to much calcium", 
+          "Flat White", 
+          "Ameriano"
+        ],
+        answer: "Flat White"
+      },
+      //8
+      {
+        question: "What type of coffee would you get if you mix: 25% espresso, and 75% steamed milk?", 
+        options: [
+          "Did you know that if you substitute water with redbull and coffee, you get a flying college student", 
+          "Pumpkin Spice Coffee", 
+          "Latte Machiato", 
+          "Carmel Machiato"
+        ],
+        answer: "Latte Machiato"
+      },
+      //9
+      {
+        question: "What type of coffee would you get if you mix: 25% whipped cream, 25% steamed milk, 25% espresso, and 25% chocolate syrup?",
+        options: [
+          "Cafe Machiato", 
+          "Kurig Coffee", 
+          "Carmel Mocha", 
+          "roses are red, violets are blue, hint- this coffee is sweet, just like you ;P"
+        ], 
+        answer: "Cafe Machiato"
+      },
+      //10
+      {
+        question: "What type of coffee would you get if you mix: 100% coffee, and 100% Ice",
+        options: [
+          "Hot cup of coffee obviously", 
+          "Iced Coffee", 
+          "Did you know the most expensive cup of coffee ever sold was in San Francisco for $1,200?", 
+          "British Coffee"
+        ],
+        answer: "Iced Coffee"
+      },
+    ],
+    currentQuestion: 0,
+    score: 0
+  };
